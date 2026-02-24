e70bc0d7825d7ac2cd7574536a11def4b70b911c
Eunice-Nina <s.eunice@alustudent.com>
2026-02-24 06:25:00 -0800

    Just done with my quests

---
+++ quests/quest_04_town_crier.py
@@ -0,0 +1,9 @@
+# Quest: Combining variables and strings for dynamic output
+
+# Create variables
+city_name = "Kigali"
+current_year = 2026
+name = "Nina"
+
+# Print dynamic message using f-string
+print(f"Welcome to {city_name}! The year is {current_year}, and our newest resident is {name}.")
